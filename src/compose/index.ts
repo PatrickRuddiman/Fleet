fc61525f4d5c9f42fa6ea54463136728bccb1e11
PatrickRuddiman <86851465+PatrickRuddiman@users.noreply.github.com>
2026-03-20 22:50:37 +0000

    feat: add compose parser types

---
+++ src/compose/index.ts
@@ -0,0 +1,1 @@
+export { NormalizedPort, ParsedService, ParsedComposeFile } from "./types";
---
+++ src/compose/types.ts
@@ -0,0 +1,14 @@
+export interface NormalizedPort {
+  published: number | null;
+  target: number;
+}
+
+export interface ParsedService {
+  hasImage: boolean;
+  hasBuild: boolean;
+  ports: NormalizedPort[];
+}
+
+export interface ParsedComposeFile {
+  services: Record<string, ParsedService>;
+}
